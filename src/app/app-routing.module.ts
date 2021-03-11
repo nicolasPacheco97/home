5cb99d2d735811a89d9f9152d83737f6c00c5a8d
nicolasPacheco <nicolaspachecol97@gmail.com>
2021-03-11 16:56:53 -0600

    Iniciando en Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component'
+import { FeedbackComponent } from './feedback/feedback.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'feedback', component: FeedbackComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
