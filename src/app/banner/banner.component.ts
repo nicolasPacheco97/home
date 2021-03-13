15a55fd529c4e3b9be6f2f2619dfa1b31075189e
nicolasPacheco <nicolaspachecol97@gmail.com>
2021-03-12 20:15:58 -0600

    Holiii

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -16,56 +16,56 @@ export class BannerComponent implements OnInit {
   images = [
     {
       id: '1',
-      link: '../../assets/images/Traje-XL.png',
+      link: 'assets/images/Traje-XL.png',
       title: 'Traje XL',
       date: '2020 01 01',
       description: 'Un traje creado como vista a la moda'
     },
     {
       id: '2',
-      link: '../../assets/images/Sueter.png',
+      link: 'assets/images/Sueter.png',
       title: 'Suéter',
       date: '2020 01 01',
       description: 'Un traje creado como vista a la moda'
     },
     {
       id: '3',
-      link: '../../assets/images/Sueter-de-estambre.png',
+      link: 'assets/images/Sueter-de-estambre.png',
       title: 'Suéter de estambre',
       date: '2020 01 01',
       description: 'Un traje creado como vista a la moda'
     },
     {
       id: '4',
-      link: '../../assets/images/Romper.png',
+      link: 'assets/images/Romper.png',
       title: 'Romper',
       date: '2020 01 01',
       description: 'Un traje creado como vista a la moda'
     },
     {
       id: '5',
-      link: '../../assets/images/Pesqueros-y-Camisa.png',
+      link: 'assets/images/Pesqueros-y-Camisa.png',
       title: 'Pesqueros y Camisa',
       date: '2020 01 01',
       description: 'Un traje creado como vista a la moda'
     },
     {
       id: '6',
-      link: '../../assets/images/Met-gala---Harry-style.png',
+      link: 'assets/images/Met-gala---Harry-style.png',
       title: 'Met Gala - Harry Styles',
       date: '2020 01 01',
       description: 'Un traje creado como vista a la moda'
     },
     {
       id: '7',
-      link: '../../assets/images/happy.png',
+      link: 'assets/images/happy.png',
       title: 'Bienvenido',
       date: '2020 01 01',
       description: 'Un traje creado como vista a la moda'
     },
     {
       id: '8',
-      link: '../../assets/images/sad.png',
+      link: 'assets/images/sad.png',
       title: 'Error 404',
       date: '2020 01 01',
       description: 'Un traje creado como vista a la moda'
